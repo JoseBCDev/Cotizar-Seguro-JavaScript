c9a075f3f6d0ecc1b3e4fdfcfe07ee8ced2ad473
xdgonex <bendezucortezjoseluis@gmail.com>
2022-09-16 00:09:56 -0500

    Comentamos el codigo

--- js/app.js
+++ js/app.js
@@ -14,16 +14,18 @@ function UI(){}
 UI.prototype.llenarOpciones = ()=>{
     const max = new Date().getFullYear();
     const min = max - 20;
-
-    const selecd = document.querySelector('#year');
+    //seleccionamos el campo selec con ID year
+    const select = document.querySelector('#year');
 
     for(let i = max; i>min; i--)
     {
+        //creamos un elemento option
         let option = document.createElement('option');
         option.value = i;
         option.textContent = i;
 
-        selecd.appendChild(option);
+        //agregamos como hijo de select
+        select.appendChild(option);
     }
 };
 
@@ -33,5 +35,5 @@ const ui = new UI();
 
 //Carge la Pagina
 document.addEventListener('DOMContentLoaded',()=>{
-   ui.llenarOpciones();
+   ui.llenarOpciones(); //Llenamos los campos de Años
 });
